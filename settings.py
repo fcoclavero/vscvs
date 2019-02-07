8fcc25f8c4b1f2780c042db05707abe384d6b716
Francisco Clavero <fcoclavero32@gmail.com>
2019-02-07 12:40:08 -0300

    Class creation methods

--- settings.py
+++ settings.py
@@ -3,14 +3,16 @@ DATA_SETS = {
         'root': 'H:\\Data\\Sketchy\\',
         'images': 'H:\\Data\\Sketchy\\rendered_256x256\\256x256\\photo\\tx_000000000000',
         'sketches': 'H:\\Data\\Sketchy\\rendered_256x256\\256x256\\sketch\\tx_000000000000',
-        'labels': 'H:\\Data\\Sketchy\\labels.pickle',
-        'dimensions': (256, 256)
+        'classes': 'H:\\Data\\Sketchy\\labels.pickle',
+        'dimensions': (256, 256),
+        'language': 'en'
     },
     'sketchy_test': {
         'root': 'C:\\Users\\Chopan\\Documents\\Data\\sketchy_test\\',
         'images': 'C:\\Users\\Chopan\\Documents\\Data\\sketchy_test\\photo',
         'sketches': 'C:\\Users\\Chopan\\Documents\\Data\\sketchy_test\\photo',
-        'labels': 'C:\\Users\\Chopan\\Documents\\Data\\sketchy_test\\labels.pickle',
-        'dimensions': (256, 256)
+        'classes': 'C:\\Users\\Chopan\\Documents\\Data\\sketchy_test\\labels.pickle',
+        'dimensions': (256, 256),
+        'language': 'en'
     }
 }
---
+++ src/create_classes.py
@@ -0,0 +1,74 @@
+import os
+import pickle
+import re
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objs as go
+
+from plotly.offline import iplot
+from sklearn.manifold import TSNE
+
+from settings import DATA_SETS
+
+from modules.textpreprocess.compound_cleaners.en import full_clean
+from modules.wordvectors.en import document_vector
+
+
+def classes_set(directory):
+    """
+    Returns a set containing all classes in a given data set. Data sets must be structured in the following fashion:
+    /data_set_name/<photos><sketches>/<classes>. In other words, they must have a root folder, inside which must be
+    a photos and a sketches folder, each containing a folder for each class. Class names are inferred from the names
+    of these directories.
+    :param directory: path to either an image or sketch folder
+    :type: str
+    :return: a set containing all class names
+    :type: str
+    """
+    return set([path for path in os.listdir(directory) if os.path.isdir(os.path.join(directory, path))])
+
+
+def plot(classes):
+    """
+    Plot a classes data frame, showing all the different classes in a 2D projection of the semantic space.
+    :param classes: a classes data frame, with "class", "vector" and "tsne" columns
+    :type: pd.DataFrame
+    :return: None
+    """
+    aux = np.vstack(classes['tsne'])
+    trace = go.Scattergl(
+        x=aux[:, 0],
+        y=aux[:, 1],
+        text=classes['class'].values,
+        mode='markers',
+        marker=dict(
+            size=16,
+            color=np.random.randn(len(aux)),
+            colorscale='Viridis'
+        )
+    )
+    data = [trace]
+    iplot(data)
+
+
+def create_classes_data_frame(data_set, tsne_dimension = 2):
+    """
+    Create a new classes data frame for the specified dataset. The dataset must be registered in the project settings.
+    The data frame is pickled before function return, to prevent re-calculating things.
+    :param data_set: the name of the dataset
+    :type: str
+    :param tsne_dimension: the dimensions for the lower dimensional vector projections
+    :type: int
+    :return: a pandas DataFrame with "class", "vector" (document embeddings) and "tsne" columns
+    :type: pd.DataFrame
+    """
+    paths = classes_set(DATA_SETS[data_set]['images']).union(classes_set(DATA_SETS[data_set]['sketches']))
+    classes = pd.DataFrame(columns=['class', 'vector', 'tsne'])
+    classes['class'] = list(paths)
+    classes['class'] = classes['class'].apply(lambda cls: ' '.join(re.split(r'(?:_|-)', cls)))
+    classes['class'] = classes['class'].apply(full_clean)
+    classes['vector'] = classes['class'].apply(document_vector)
+    classes['tsne'] = list(TSNE(n_components=tsne_dimension).fit_transform(np.vstack(classes['vector'].values)))
+    pickle.dump(classes, open(DATA_SETS[data_set]['classes'], 'wb'))
+    return classes

f49fca606a420f608ddc0b208b2c8280c6745d9c
Francisco Clavero <fcoclavero32@gmail.com>
2019-03-02 11:06:01 -0300

    Image encoder.

--- src/models/generators/encoder_images.py
+++ src/models/generators/encoder_images.py
@@ -1,2 +1,51 @@
-# In the DCGAN paper, the authors specify that all model weights are be randomly initialized from a Normal
-# distribution with mean=0, stdev=0.2.
+import torch.nn as nn
+
+
+class ImageEncoder(nn.Module):
+    def __init__(self, output_dimension, n_channels, feature_depth, n_gpu):
+        """
+        Convolutional network that creates feature vectors for images.
+        :param output_dimension: the size of the output vector (common vector space dimensionality)
+        :type: int
+        :param n_channels: number of channels of input images
+        :type: int
+        :param feature_depth: regulates the size of channels in the convolutional layers
+        :type: int
+        :param n_gpu: number of available gpus for training
+        :type: int
+        :return the image encoder pytorch model
+        :type: pytorch.nn.Module
+        """
+        super(ImageEncoder, self).__init__()
+        self.ngpu = n_gpu
+        self.main = nn.Sequential(
+            # input is (nc) x 256 x 256
+            nn.Conv2d(n_channels, feature_depth, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ngf) x 128 x 128
+            nn.Conv2d(feature_depth, feature_depth * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_depth * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ngf*2) x 64 x 64
+            nn.Conv2d(feature_depth * 2, feature_depth * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_depth * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ngf*4) x 32 x 32
+            nn.Conv2d(feature_depth * 4, feature_depth * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_depth * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ngf*8) x 16 x 16
+            nn.Conv2d(feature_depth * 8, feature_depth * 16, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_depth * 16),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ngf*16) x 8 x 8
+            nn.Conv2d(feature_depth * 16, feature_depth * 32, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_depth * 32),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ngf*32) x 4 x 4
+            nn.Conv2d(feature_depth * 32, output_dimension, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
--- src/utils/initialize_weights.py
+++ src/utils/initialize_weights.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 
-def weights_init(model, conv_mean=0.2, conv_std=0.0, batch_norm_mean=0.2, batch_norm_std=1.0, batch_norm_bias=0.0):
+def initialize_weights(model, conv_mean=0.2, conv_std=0.0, batch_norm_mean=0.2, batch_norm_std=1.0, batch_norm_bias=0.0):
     """
     Custom weights initialization.
     The function takes an initialized model as input and re-initializes all convolutional, convolutional-transpose,
@@ -11,10 +11,15 @@ def weights_init(model, conv_mean=0.2, conv_std=0.0, batch_norm_mean=0.2, batch_
     :param model: the model to be initialized
     :type: torch.nn.Module
     :param conv_mean: the mean for the Normal distribution for the convolutional layer weights
+    :type: float
     :param conv_std: the standard deviation for the Normal distribution for the convolutional layer weights
+    :type: float
     :param batch_norm_mean: the mean for the Normal distribution for the batch normalization layer weights
+    :type: float
     :param batch_norm_std: the standard deviation for the Normal distribution for the batch normalization layer weights
+    :type: float
     :param batch_norm_bias: constant initial value for the batch normalization bias layer weights
+    :type: float
     :return: None
     """
     classname = model.__class__.__name__

b49f6ff73aceedea2ddd4d7df8313dab261eb786
Francisco Clavero <fcoclavero32@gmail.com>
2019-03-20 18:29:15 -0300

    Refactored model script names and added intermodal discriminatoris.

--- src/models/discriminators/intermodal_onehot.py
+++ src/models/discriminators/intermodal_onehot.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
 
 
-class InterModalDiscriminator(nn.Module):
+class InterModalDiscriminatorOneHot(nn.Module):
     def __init__(self, input_dimension, n_gpu):
         """
         Fully connected network that classifies vectors in the common vector space as belonging any mode
-        (image or sketch).
+        (image or sketch). Uses one-hot encoded vectors.
         :param input_dimension: the size of the input vector (common vector space dimensionality)
         :type: int
         :param n_gpu: number of available gpus for training
@@ -13,7 +13,7 @@ class InterModalDiscriminator(nn.Module):
         :return the image encoder pytorch model
         :type: pytorch.nn.Module
         """
-        super(InterModalDiscriminator, self).__init__()
+        super(InterModalDiscriminatorOneHot, self).__init__()
         self.n_gpu = n_gpu
         self.main = nn.Sequential(
             # state size. input_dimension
@@ -23,8 +23,8 @@ class InterModalDiscriminator(nn.Module):
             nn.Linear(75, 25),
             # state size. 25
             nn.ReLU(),
-            nn.Linear(25, 1), # binary output
-            nn.Sigmoid() # 0 < output_value < 1
+            nn.Linear(25, 2),  # binary one-hot encoding vector
+            nn.Softmax(dim=1) # values must sum 1 on dimension 1, that is for the 2D outputs for each example
         )
 
     def forward(self, input):

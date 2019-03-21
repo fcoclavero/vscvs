fdc0447a8798a872f2e3d1eb4ba24359f661e17b
Francisco Clavero <fcoclavero32@gmail.com>
2019-03-21 18:15:48 -0300

    Refactored imports in train.py

--- src/train.py
+++ src/train.py
@@ -2,9 +2,6 @@ import click
 
 from settings import DATA_SETS
 
-from src.trainers.sketchy_cnn import train_sketchy_cnn
-from src.trainers.cvs_gan import train_cvs_gan
-
 
 @click.group()
 def train():
@@ -14,11 +11,13 @@ def train():
 
 @train.command()
 def sketchy_cnn():
+    from src.trainers.sketchy_cnn import train_sketchy_cnn
     click.echo('sketchy cnn')
     train_sketchy_cnn(DATA_SETS['sketchy_test']['images'], DATA_SETS['sketchy_test']['dimensions'][0])
 
 
 @train.command()
 def cvs_gan():
+    from src.trainers.cvs_gan import train_cvs_gan
     click.echo('sketchy cnn')
     train_cvs_gan(DATA_SETS['sketchy_test']['images'], DATA_SETS['sketchy_test']['dimensions'][0])

3a92c79f2013cc45ba3faccfe08d9ec55292820f
Francisco Clavero <fcoclavero32@gmail.com>
2019-03-26 15:49:10 -0300

    Multiclass precision.

--- src/metrics/multi_class.py
+++ src/metrics/multi_class.py
@@ -9,9 +9,6 @@ class Accuracy(AbstractAccuracy):
     """
     Accuracy for multi-class classification.
     """
-    def __init__(self):
-        super(Accuracy, self).__init__()
-
     def __call__(self, output, target, loss):
         prediction = output.argmax(1)
         self.correct += (prediction == target).sum().float()
@@ -19,19 +16,47 @@ class Accuracy(AbstractAccuracy):
         return self.value
 
 
-class MeanAveragePrecision(AbstractMetric):
+class AveragePrecision(AbstractMetric):
     """
-    Mean precision over all classes: mean(TP_i / (TP_i + FP_i)) for i in classes.
+    Average precision over all classes: mean(TP_i / (TP_i + FP_i)) for i in classes.
     """
     def __init__(self):
-        super(MeanAveragePrecision, self).__init__()
+        super(AveragePrecision, self).__init__()
         self.average_precision = 0
-        self.total = 0
 
     def __call__(self, output, target, loss):
         prediction = output.argmax(1)
+        # cm[i][i] are the tp for class i, sum of column i are all the positives for class i
         cm = confusion_matrix(target, prediction)
-        self.average_precision += np.mean(np.diag(cm) / np.sum(cm, axis=0)) # TODO: watch out for zero divide when a class is not predicted
+        # Map over true positives and positives and compute precision as tp / p.
+        precisions = map(lambda tp, p: 0.0 if p == 0 else tp / p, np.diag(cm), np.sum(cm, axis=0))
+        self.average_precision = np.mean(list(precisions)) # Get the mean over all classes
+        return self.value
+
+    @property
+    def name(self):
+        return 'AP'
+
+    @property
+    def value(self):
+        return self.average_precision
+
+    def reset(self):
+        self.average_precision = 0
+
+
+class MeanAveragePrecision(AveragePrecision):
+    """
+    Mean, over all experiments, of the average precision of all classes: mean(TP_i / (TP_i + FP_i)) for i in classes.
+    """
+    def __init__(self):
+        super(MeanAveragePrecision, self).__init__()
+        self.sum = 0.0
+        self.total = 0.0
+
+    def __call__(self, output, target, loss):
+        super(MeanAveragePrecision, self).__call__(output, target, loss)
+        self.sum += self.average_precision
         self.total += 1
         return self.value
 
@@ -41,8 +66,9 @@ class MeanAveragePrecision(AbstractMetric):
 
     @property
     def value(self):
-        return self.average_precision / self.total
+        return 0.0 if self.total == 0 else self.sum / self.total
 
     def reset(self):
-        self.average_precision = 0
+        super(MeanAveragePrecision, self).reset()
+        self.sum = 0
         self.total = 0
--- test/models/discriminators/test_intermodal.py
+++ test/models/discriminators/test_intermodal.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn.parallel
 
 from settings import DATA_SETS
-from src.metrics.multi_class import Accuracy
+from src.metrics.multi_class import Accuracy, MeanAveragePrecision, AveragePrecision
 from src.models.discriminators.intermodal import InterModalDiscriminatorOneHot
 from src.utils.data import split
 
@@ -45,7 +45,7 @@ def test_onehot_classification(n_gpu = 1):
     print(net)
 
     # Define metrics
-    metrics = [Accuracy()]
+    metrics = [Accuracy(), AveragePrecision(), MeanAveragePrecision()]
 
     epoch_size = 10
     epochs = 10

2568b9923234d0825427f74f7f53925659599a27
Francisco Clavero <fcoclavero32@gmail.com>
2019-04-02 12:14:24 -0300

    Refactored Sketchy dataset.

--- src/datasets/sketchy.py
+++ src/datasets/sketchy.py
@@ -4,13 +4,23 @@ import torchvision.transforms as transforms
 from settings import DATA_SETS
 
 
-class SketchyImages(ImageFolder):
-    def __init__(self):
-        super(SketchyImages, self).__init__(
-            root=DATA_SETS['sketchy_test']['images'],
+class Sketchy(ImageFolder):
+    """
+    Utility class for loading the sketchy dataset. It's original structure is compatible with
+    the torch ImageFolder, so I will just subclass that and apply some transforms.
+    """
+    def __init__(self, dataset):
+        """
+        Initialize the ImageFolder and perform transforms. Note that sketches and images have the
+        same exact dimension in both the sketchy and sketchy_test datasets.
+        :param dataset: the root dir for images or sketches.
+        :type: str
+        """
+        super(Sketchy, self).__init__(
+            root=dataset,
             transform=transforms.Compose([
-                transforms.Resize(DATA_SETS['sketchy_test']['dimensions'][0]),
-                transforms.CenterCrop(DATA_SETS['sketchy_test']['dimensions'][0]),
+                transforms.Resize(DATA_SETS['sketchy']['dimensions'][0]),
+                transforms.CenterCrop(DATA_SETS['sketchy']['dimensions'][0]),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
             ])

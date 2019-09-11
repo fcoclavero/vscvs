f5ae0759a84404542572c1f3ee0c3cdc55850118
Francisco Clavero <fcoclavero32@gmail.com>
2019-09-11 18:44:45 -0300

    More kernel convolutions.

--- src/models/gradients.py
+++ src/models/gradients.py
@@ -30,10 +30,34 @@ class AbstractKernelConvolution(torch.nn.Module):
 class SobelX(AbstractKernelConvolution):
     """
     Torch nn Layer for the Sobel operator along the x axis. It approximates the gradient along the x axis by
-    filtering (convolution) the input with a specific kernel. It only works with grayscale images.
+    filtering (convolution) the input with a specific kernel. It only works with grayscale images. Sobel kernel size 1.
     Reference: https://en.wikipedia.org/wiki/Sobel_operator
     """
     @property
     def kernel(self):
         kernel = torch.tensor([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=torch.float)
+        return kernel[None, None] # kernel was defined as 2D, so we must `unsqueeze(self.kernel, 0)` twice
+
+
+class SobelY(AbstractKernelConvolution):
+    """
+    Torch nn Layer for the Sobel operator along the y axis. It approximates the gradient along the y axis by
+    filtering (convolution) the input with a specific kernel. It only works with grayscale images. Sobel kernel size 1.
+    Reference: https://en.wikipedia.org/wiki/Sobel_operator
+    """
+    @property
+    def kernel(self):
+        kernel = torch.tensor([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], dtype=torch.float)
+        return kernel[None, None] # kernel was defined as 2D, so we must `unsqueeze(self.kernel, 0)` twice
+
+
+class Laplacian(AbstractKernelConvolution):
+    """
+    Torch nn Layer for the Laplacian derivative operator. It approximates the gradient magnitude along both axes by
+    filtering (convolution) the input with a specific kernel. It only works with grayscale images. Kernel size 1.
+    Reference: https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_gradients/py_gradients.html
+    """
+    @property
+    def kernel(self):
+        kernel = torch.tensor([[0, 1, 0], [1, 4, 1], [0, 1, 0]], dtype=torch.float)
         return kernel[None, None] # kernel was defined as 2D, so we must `unsqueeze(self.kernel, 0)` twice

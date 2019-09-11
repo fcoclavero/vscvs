f6131d8e08b307fc7a608c00e4b91645d5878670
Francisco Clavero <fcoclavero32@gmail.com>
2019-09-11 17:51:29 -0300

    Refactoring to easily implement more kernel convolution modules.

--- src/models/gradients.py
+++ src/models/gradients.py
@@ -3,16 +3,13 @@ import torch
 import torch.nn.functional as F
 
 
-class SobelX(torch.nn.Module):
-    def __init__(self):
-        """
-        Torch nn Layer for the Sobel operator along the x axis. It approximates the gradient along the x axis by
-        filtering (convolution) the input with a specific kernel. It only works with grayscale images.
-        Reference: https://en.wikipedia.org/wiki/Sobel_operator
-        """
-        super(SobelX, self).__init__()
-        self.kernel = torch.tensor([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=torch.float)
-        self.kernel = self.kernel[None, None] # kernel was defined as 2D, so we must `unsqueeze(self.kernel, 0)` twice
+class AbstractKernelConvolution(torch.nn.Module):
+    """ Abstract nn.Module for creating convolutions with user defined kernels. """
+
+    @property
+    def kernel(self):
+        """ Get the custom kernel for the implementation of the kernel convolution. """
+        raise NotImplementedError
 
     def forward(self, x):
 
@@ -28,3 +25,15 @@ class SobelX(torch.nn.Module):
     def __call__(self, *input, **kwargs):
         """ Make the object callable by executing the `forward` function`. """
         return self.forward(*input, **kwargs)
+
+
+class SobelX(AbstractKernelConvolution):
+    """
+    Torch nn Layer for the Sobel operator along the x axis. It approximates the gradient along the x axis by
+    filtering (convolution) the input with a specific kernel. It only works with grayscale images.
+    Reference: https://en.wikipedia.org/wiki/Sobel_operator
+    """
+    @property
+    def kernel(self):
+        kernel = torch.tensor([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=torch.float)
+        return kernel[None, None] # kernel was defined as 2D, so we must `unsqueeze(self.kernel, 0)` twice

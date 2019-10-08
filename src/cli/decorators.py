c2754ec9a8d71ccdd785b03568dd9bc4b5f6fed8
Francisco Clavero <fcoclavero32@gmail.com>
2019-10-07 22:39:03 -0300

    Refactored embed CLI to allow shared parameters between different commands. Added the train_test_split parameter to these.

--- src/cli/decorators.py
+++ src/cli/decorators.py
@@ -10,7 +10,7 @@ import click
 import functools
 
 
-def pass_kwargs_in_context(func):
+def pass_kwargs_to_context(func):
     """
     Decorator for `click` CLIs that puts all the kwargs of the decorated function to the click context and passes it
     on with `click.pass_context`. This is useful when multiple commands receive the same objects. In this case, a click
@@ -25,5 +25,22 @@ def pass_kwargs_in_context(func):
     def wrapper(context, *args, **kwargs):
         """ Extend the context dict with the provided kwargs. """
         context.obj = {**context.obj, **kwargs} if context.obj else kwargs
+        return context.invoke(func, context.obj, *args, **kwargs)
+    return functools.update_wrapper(wrapper, func)
+
+
+def pass_context_to_kwargs(func):
+    """
+    Decorator for `click` CLIs that puts all the kwargs in the click context in the decorated function's invocation
+    kwargs. This can be a more elegant way to receive the context in some cases.
+    :param func: the function to be decorated
+    :type: function
+    :return: the decorated function, which passes all kwargs in the click context to the invocation kwargs
+    :type: function
+    """
+    @click.pass_context # needed to receive the context
+    def wrapper(context, *args, **kwargs):
+        """ Extend kwargs with the context dict parameters. """
+        kwargs = {**context.obj, **kwargs} if context.obj else kwargs
         return context.invoke(func, context.obj, *args, **kwargs)
     return functools.update_wrapper(wrapper, func)
--- src/cli/embed.py
+++ src/cli/embed.py
@@ -10,30 +10,34 @@ import click
 import os
 import torch
 
-from src.utils.embeddings import create_embeddings
 from settings import ROOT_DIR
+from src.cli.decorators import pass_context_to_kwargs, pass_kwargs_to_context
+from src.utils.embeddings import create_embeddings
+
 
 @click.group()
-def embed():
+@click.option('--dataset_name', prompt='Dataset name', help='The name of the dataset to be used for training.',
+              type=click.Choice(['sketchy_photos', 'sketchy_sketches', 'sketchy_test_photos', 'sketchy_test_sketches']))
+@click.option('--train_test_split', prompt='Train/test split', default=1.0,
+              help='Proportion of the dataset to be used for training.')
+@click.option('--embeddings_name', prompt='Embeddings name', help='Name of file where the embeddings will be saved.')
+@click.option('--batch_size', prompt='Batch size', help='The batch size for the embedding routine.', default=16)
+@click.option('--workers', prompt='Data loader workers', help='The number of workers for the data loader.', default=4)
+@click.option('--n_gpu', prompt='Number of gpus', help='The number of GPUs available. Use 0 for CPU mode.', default=0)
+@pass_kwargs_to_context
+def embed(*args, **kwargs):
     """ Image embedding creation click group. """
     pass
 
 
 @embed.command()
-@click.option(
-    '--dataset_name', prompt='Dataset name', help='The name of the dataset to be used for training.',
-    type=click.Choice(['sketchy_photos', 'sketchy_sketches', 'sketchy_test_photos', 'sketchy_test_sketches'])
-)
-@click.option('--embeddings_name', prompt='Embeddings name', help='Name of file where the embeddings will be saved.')
+@pass_context_to_kwargs
 @click.option('--in_channels', prompt='In channels', help='Number of image color channels.', default=3)
 @click.option('--cell_size', prompt='Cell size', help='Gradient pooling size.', default=8)
 @click.option('--n_bins', prompt='Number of histogram bins', help='Number of histogram bins.', default=9)
 @click.option('--signed_gradients', prompt='Signed gradients', help='Use signed gradients?', default=False)
-@click.option('--batch_size', prompt='Batch size', help='The batch size for the embedding routine.', default=16)
-@click.option('--workers', prompt='Data loader workers', help='The number of workers for the data loader.', default=4)
-@click.option('--n_gpu', prompt='Number of gpus', help='The number of GPUs available. Use 0 for CPU mode.', default=0)
-def hog(dataset_name, embeddings_name, in_channels, cell_size, n_bins, signed_gradients,
-        batch_size, workers, n_gpu):
+def hog(_, dataset_name, train_test_split, embeddings_name, batch_size, workers, n_gpu,
+        in_channels, cell_size, n_bins, signed_gradients):
     click.echo('HOG embeddings for {} dataset'.format(dataset_name))
     from src.models.hog import HOG
     model = HOG(in_channels, cell_size, n_bins, signed_gradients)
@@ -41,17 +45,10 @@ def hog(dataset_name, embeddings_name, in_channels, cell_size, n_bins, signed_gr
 
 
 @embed.command()
-@click.option(
-    '--dataset_name', prompt='Dataset name', help='The name of the dataset to be used for training.',
-    type=click.Choice(['sketchy_photos', 'sketchy_sketches', 'sketchy_test_photos', 'sketchy_test_sketches'])
-)
-@click.option('--embeddings_name', prompt='Embeddings name', help='Name of file where the embeddings will be saved.')
+@pass_context_to_kwargs
 @click.option('--checkpoint', prompt='Checkpoint date', help='Checkpoint date (corresponds to its directory name.')
 @click.option('--epoch', prompt='Checkpoint epoch', help='Epoch corresponding to the model state to be loaded.')
-@click.option('--batch_size', prompt='Batch size', help='The batch size for the embedding routine.', default=16)
-@click.option('--workers', prompt='Data loader workers', help='The number of workers for the data loader.', default=4)
-@click.option('--n_gpu', prompt='Number of gpus', help='The number of GPUs available. Use 0 for CPU mode.', default=0)
-def cnn(dataset_name, embeddings_name, checkpoint, epoch, batch_size, workers, n_gpu):
+def cnn(_, dataset_name, train_test_split, embeddings_name, batch_size, workers, n_gpu, checkpoint, epoch):
     click.echo('CNN embeddings for {} dataset'.format(dataset_name))
     # Load the model checkpoint
     checkpoint_directory = os.path.join(ROOT_DIR, 'data', 'checkpoints', 'cnn', checkpoint)

fe2c7150df677234bf640519c4648c23c0446153
Francisco Clavero <fcoclavero32@gmail.com>
2019-10-29 18:36:55 -0300

    GCN using torch-geometric

--- src/models/graph_convolutional_network.py
+++ src/models/graph_convolutional_network.py
@@ -6,41 +6,23 @@ __status__ = 'Prototype'
 """ Graph convolutional network PyTorch modules. """
 
 
-import math
 import torch
 
-from torch.nn.parameter import Parameter
-from torch.nn.modules.module import Module
+import torch.nn.functional as F
+
+from torch_geometric.nn import GCNConv
 
 
-class GraphConvolution(Module):
-    """
-    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
-    """
-    def __init__(self, in_features, out_features, bias=True):
-        super().__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
-        if bias:
-            self.bias = Parameter(torch.FloatTensor(out_features))
-        else:
-            self.register_parameter('bias', None)
-        self.reset_parameters()
+class GCN(torch.nn.Module):
+    def __init__(self):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(125, 125)
+        self.conv2 = GCNConv(125, 125)
 
-    def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.weight.size(1))
-        self.weight.data.uniform_(-stdv, stdv)
-        if self.bias is not None:
-            self.bias.data.uniform_(-stdv, stdv)
-
-    def forward(self, input, adj):
-        support = torch.mm(input, self.weight)
-        output = torch.spmm(adj, support)
-        if self.bias is not None:
-            return output + self.bias
-        else:
-            return output
-
-    def __repr__(self):
-        return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)

613ce1abb9e7e051580145cdb3af6c0cfd60e636
Francisco Clavero <fcoclavero32@gmail.com>
2019-11-21 12:22:19 -0300

    Setip and cleanup scripts for Gradient.

---
+++ ci/gradient/cleanup.sh
@@ -0,0 +1,5 @@
+ls storage/vscvs/data/embeddings
+apt-get update && \
+apt-get install rsync && \
+rsync --recursive --update data storage/vscvs
+ls storage/vscvs/data/embeddings
---
+++ ci/gradient/setup.sh
@@ -0,0 +1,3 @@
+pip install -r requirements.py
+pip install -r modules/textpreprocess/requirements.py
+pip install -r modules/wordvectors/requirements.py

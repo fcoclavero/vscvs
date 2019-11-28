0f3d4ea54c737a4470f17d1b5db3a5897f82c201
Francisco Clavero <fcoclavero32@gmail.com>
2019-11-27 21:39:57 -0300

    test

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,11 @@
 print('print from python')
 
+import os
 from dotenv import load_dotenv
-
 
 # Load env
 load_dotenv()
+
+print('DATA_DIR={}'.format(os.environ['DATA_DIR']))
+
+print('end of python file')

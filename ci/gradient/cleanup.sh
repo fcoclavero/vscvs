9f3398853e1ed23772c9611cd48d7ab4626a507c
Francisco Clavero <fcoclavero32@gmail.com>
2019-11-27 21:59:15 -0300

    Optimized Dockerfile.

--- ci/gradient/cleanup.sh
+++ ci/gradient/cleanup.sh
@@ -1,7 +1,5 @@
 echo cleanup
 ls storage/vscvs/data/embeddings
-apt-get update && \
-apt-get install rsync && \
 rsync --recursive --update data storage/vscvs
 ls storage/vscvs/data/embeddings
 echo cleanup complete
--- ci/gradient/jobs/hello_world.ps1
+++ ci/gradient/jobs/hello_world.ps1
@@ -1,4 +1,4 @@
 gradient jobs create `
 --name hello_world `
 --optionsFile config.yaml `
---command 'echo hello_world'
+--command 'echo hello_world && printenv'

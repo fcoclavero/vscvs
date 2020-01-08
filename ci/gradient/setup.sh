50a2dfcc287c53e06d385788e5ddbfd6212592d3
Francisco Clavero <fcoclavero32@gmail.com>
2020-01-08 16:07:38 -0300

    Test

--- ci/gradient/setup.sh
+++ ci/gradient/setup.sh
@@ -1,6 +1,6 @@
 echo INFO: setup
 cp /storage/vscvs.env .env
-pip install --upgrade tensorboard && pip install --upgrade torch
+#pip install --upgrade tensorboard && pip install --upgrade torch
 nvcc --version
 nvidia-smi
 pip freeze

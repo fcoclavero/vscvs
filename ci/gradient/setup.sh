7a2802d6db8d2e35f6c97ab6e3b6b72951452b0d
Francisco Clavero <fcoclavero32@gmail.com>
2020-01-13 18:45:17 -0300

    Test

--- ci/gradient/setup.sh
+++ ci/gradient/setup.sh
@@ -2,5 +2,5 @@ echo INFO: setup
 cp /storage/vscvs.env .env
 nvcc --version
 nvidia-smi
-ls -la /storage/data/sketchy/sketch/tx_000000000000/apple/
+python -c "from PIL import Image; Image.open('/storage/data/sketchy/sketch/tx_000000000000/apple/n07739125_1406-6.png')"
 echo INFO: setup complete

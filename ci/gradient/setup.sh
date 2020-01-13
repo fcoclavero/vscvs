fc18557eaa0a9c2050f6d949c52bed4b83554f95
Francisco Clavero <fcoclavero32@gmail.com>
2020-01-13 17:29:04 -0300

    TEst.

--- ci/gradient/setup.sh
+++ ci/gradient/setup.sh
@@ -1,8 +1,5 @@
 echo INFO: setup
 cp /storage/vscvs.env .env
-sudo apt-get install wget
-conda install -c conda-forge cudatoolkit-dev
-pip install --upgrade torch
 nvcc --version
 nvidia-smi
 echo INFO: setup complete

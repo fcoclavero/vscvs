4e67d17d1eef5376775c667389add28859096fe5
Francisco Clavero <fcoclavero32@gmail.com>
2020-01-13 17:46:36 -0300

    test

--- ci/gradient/setup.sh
+++ ci/gradient/setup.sh
@@ -2,4 +2,5 @@ echo INFO: setup
 cp /storage/vscvs.env .env
 nvcc --version
 nvidia-smi
+ls /storage/data/sketchy/sketch/tx_000000000000/apple/ | grep n07739125_1406-6.png
 echo INFO: setup complete

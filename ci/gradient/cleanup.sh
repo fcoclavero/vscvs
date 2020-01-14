ae99eaecbf9924a0ecbc3b8bb3a2359c3ff821f8
Francisco Clavero <fcoclavero32@gmail.com>
2020-01-14 18:50:42 -0300

    dbxcli sync on job cleanup

--- ci/gradient/cleanup.sh
+++ ci/gradient/cleanup.sh
@@ -1,4 +1,4 @@
 echo INFO: cleanup
 rsync --recursive --update data /storage/vscvs
-ls
+find data | while read file; do echo "$file"; target="Workspace/Python/Tesis/paperspace/"$file; /storage/other/dbxcli put "$file" "$target"; done
 echo INFO: cleanup complete

3d44141d50b10c5bffd9d6370dcc2cf287c0eed9
Francisco Clavero <fcoclavero32@gmail.com>
2020-01-14 20:05:24 -0300

    dbx upload does not recognize credentials, so fuck it

--- ci/gradient/cleanup.sh
+++ ci/gradient/cleanup.sh
@@ -1,4 +1,3 @@
 echo INFO: cleanup
 rsync --recursive --update data /storage/vscvs
-find data | while read file; do echo "$file"; target="Workspace/Python/Tesis/paperspace/"$file; /storage/other/dbxcli put "$file" "$target"; done
 echo INFO: cleanup complete

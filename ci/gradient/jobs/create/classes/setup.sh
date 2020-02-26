451f018fee0185ea5e51b21df22a3bc55b3c5fe7
Francisco Clavero <fcoclavero32@gmail.com>
2020-02-26 13:42:06 -0300

    test

--- ci/gradient/jobs/create/classes/setup.sh
+++ ci/gradient/jobs/create/classes/setup.sh
@@ -1,1 +1,1 @@
-python -c "import nltk; nltk.download('wordnet'); nltk.download('punkt'); nltk.download('stopwords')"
+python -c "import nltk; nltk.download('averaged_perceptron_tagger'); nltk.download('wordnet'); nltk.download('punkt'); nltk.download('stopwords')"

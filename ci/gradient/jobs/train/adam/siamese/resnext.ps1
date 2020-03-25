87fb8169647d7ec020c6caa331589805152c7f56
Francisco Clavero <fcoclavero32@gmail.com>
2020-03-25 16:48:27 -0300

    Added `workspaceRef` parameter in gradient config yml to launch experiments from the `dev` branch.

--- ci/gradient/jobs/train/adam/siamese/resnext.ps1
+++ ci/gradient/jobs/train/adam/siamese/resnext.ps1
@@ -5,6 +5,6 @@ gradient jobs create `
     'sh ci/gradient/setup.sh && ' + `
     'python main.py train --train-validation-split 0.9 --batch-size 16 --epochs 50 --workers 16 --n-gpu 1 ' + `
                          'adam --learning-rate 0.005 --beta-1 0.9 --beta-2 0.999 --epsilon 0.00000001 --weight-decay 0.0001 --amsgrad True ' + `
-                         'siamese --dataset-name sketchy --margin 0.2 resnext && ' + `
+                         'siamese --dataset-name sketchy --margin 0.6 resnext && ' + `
     'sh ci/gradient/cleanup.sh' `
 )

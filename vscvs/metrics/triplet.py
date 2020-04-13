* Fixed triplet `AverageDistance` metric. * Added more options for weight sharing in Triplet models.

vscvs/metrics/triplet.py
@@ -216,7 +216,7 @@ class AverageDistances(TripletMetric):
         """
         anchor_embeddings, positive_embeddings, negative_embeddings = output
         batch_size = self._batch_size(anchor_embeddings)
-        self._sum_positive = torch.nn.functional.pairwise_distance(anchor_embeddings, positive_embeddings).pow(2)
-        self._sum_negative = torch.nn.functional.pairwise_distance(anchor_embeddings, negative_embeddings).pow(2)
+        self._sum_positive = torch.nn.functional.pairwise_distance(anchor_embeddings, positive_embeddings).pow(2).sum()
+        self._sum_negative = torch.nn.functional.pairwise_distance(anchor_embeddings, negative_embeddings).pow(2).sum()
         self._num_examples_negative += batch_size
         self._num_examples_positive += batch_size

vscvs/models/triplet.py
@@ -13,23 +13,24 @@ class TripletNetwork(nn.Module):
     """
     Pytorch module for a triplet network.
     Triplet networks produce similar embeddings for similar inputs by training using a triplet loss function that
-    receives three input examples, an anchor, a positive (same class as anchor) and a negative (different class thant
+    receives three input examples, an anchor, a positive (same class as anchor) and a negative (different class than
     anchor's). It penalizes similar embeddings for the anchor and the negative, as well as dissimilar embeddings for the
     anchor and the positive.
-    Positive and negative elements in each triplet are assumed to be of the same mode, and thus their embedding network
-    will share weights.
     """
-    def __init__(self, anchor_embedding_network, positive_negative_embedding_network):
+    def __init__(self, anchor_embedding_network, positive_embedding_network, negative_embedding_network):
         """
         Model constructor.
         :param anchor_embedding_network: the network that will encode the anchor element of each triplet.
         :type: torch.nn.module
-        :param positive_negative_embedding_network: the network that will encode the positive and negative elements.
+        :param positive_embedding_network: the network that will encode the positive elements.
+        :type: torch.nn.module
+        :param negative_embedding_network: the network that will encode the negative elements.
         :type: torch.nn.module
         """
         super().__init__()
         self.anchor_embedding_network = anchor_embedding_network
-        self.positive_negative_embedding_network = positive_negative_embedding_network
+        self.positive_embedding_network = positive_embedding_network
+        self.negative_embedding_network = negative_embedding_network
 
     def forward(self, anchor, positive, negative):
         """
@@ -44,6 +45,67 @@ class TripletNetwork(nn.Module):
         :type: torch.Tensor, torch.Tensor
         """
         anchor_embedding = self.anchor_embedding_network(anchor)
-        positive_embedding = self.positive_negative_embedding_network(positive)
-        negative_embedding = self.positive_negative_embedding_network(negative)
+        positive_embedding = self.positive_embedding_network(positive)
+        negative_embedding = self.negative_embedding_network(negative)
         return anchor_embedding, positive_embedding, negative_embedding
+
+
+class TripletSharedAnchorPositive(TripletNetwork):
+    """
+    Triplet network with shared embedding network weights for anchor and positive triplet elements.
+    """
+    def __init__(self, anchor_positive_embedding_network, negative_embedding_network):
+        """
+        Model constructor.
+        :param anchor_positive_embedding_network: the network that will encode anchor and positive elements.
+        :type: torch.nn.module
+        :param negative_embedding_network: the network that will encode the negative elements.
+        :type: torch.nn.module
+        """
+        super().__init__(
+            anchor_positive_embedding_network, anchor_positive_embedding_network, negative_embedding_network)
+
+
+class TripletSharedAnchorNegative(TripletNetwork):
+    """
+    Triplet network with shared embedding network weights for anchor and negative triplet elements.
+    """
+    def __init__(self, anchor_negative_embedding_network, positive_embedding_network):
+        """
+        Model constructor.
+        :param anchor_negative_embedding_network: the network that will encode anchor and negative elements.
+        :type: torch.nn.module
+        :param positive_embedding_network: the network that will encode the positive elements.
+        :type: torch.nn.module
+        """
+        super().__init__(
+            anchor_negative_embedding_network, positive_embedding_network, anchor_negative_embedding_network)
+
+
+class TripletSharedPositiveNegative(TripletNetwork):
+    """
+    Triplet network with shared embedding network weights for positive and negative triplet elements.
+    """
+    def __init__(self, anchor_embedding_network, positive_negative_embedding_network):
+        """
+        Model constructor.
+        :param anchor_embedding_network: the network that will encode the anchor elements of each triplet.
+        :type: torch.nn.module
+        :param positive_negative_embedding_network: the network that will encode positive and negative elements.
+        :type: torch.nn.module
+        """
+        super().__init__(
+            anchor_embedding_network, positive_negative_embedding_network, positive_negative_embedding_network)
+
+
+class TripletShared(TripletNetwork):
+    """
+    Triplet network with shared embedding network weights for all triplet elements.
+    """
+    def __init__(self, embedding_network):
+        """
+        Model constructor.
+        :param embedding_network: the network that will encode all elements of each triplet.
+        :type: torch.nn.module
+        """
+        super().__init__(embedding_network, embedding_network, embedding_network)

vscvs/trainers/triplet.py
@@ -11,7 +11,7 @@ from torch.utils.data._utils.collate import default_collate
 
 from vscvs.loss_functions import TripletLoss
 from vscvs.metrics.triplet import Accuracy, AverageDistances, Loss
-from vscvs.models import CNNNormalized, ResNetNormalized, ResNextNormalized, TripletNetwork
+from vscvs.models import CNNNormalized, ResNetNormalized, ResNextNormalized, TripletSharedPositiveNegative
 from vscvs.trainers.abstract_trainer import AbstractTrainer
 from vscvs.trainers.engines.triplet import create_triplet_evaluator, create_triplet_trainer
 from vscvs.utils.collators import triplet_collate
@@ -54,7 +54,7 @@ class AbstractTripletTrainer(AbstractTrainer, ABC):
 
     @property
     def initial_model(self):
-        return TripletNetwork(self.anchor_network, self.positive_negative_network)
+        return TripletSharedPositiveNegative(self.anchor_network, self.positive_negative_network)
 
     @property
     def loss(self):
4ffa6ff77adde10894490427271977c2b3c73b13
Francisco Clavero <fcoclavero32@gmail.com>
2020-05-22 00:11:10 -0400

    `sketchy_test_dataset` command.

--- vscvs/cli/create.py
+++ vscvs/cli/create.py
@@ -7,6 +7,9 @@ __status__ = 'Prototype'
 
 
 import click
+import os
+
+from tqdm import tqdm
 
 
 @click.group()
@@ -46,3 +49,40 @@ def sample_vectors(n, dimension):
     click.echo('Creating sample vectors.')
     from vscvs.preprocessing import create_sample_vectors
     create_sample_vectors(n, dimension)
+
+
+@create.command()
+@click.option('--photos-per-class', default=10, help='Number of photo image instances per class to be included.')
+@click.option('--photos-root', prompt='Photos root', type=click.Path(exists=True),
+              help='Directory containing the sketchy photo image files.')
+@click.option('--sketches-root', prompt='Sketches root', type=click.Path(exists=True),
+              help='Directory containing the sketchy sketch image files.')
+@click.option('--test-photos-root', prompt='Test photos root', type=click.Path(exists=True),
+              help='Directory containing the sketchy test photo image files.')
+@click.option('--test-sketches-root', prompt='Test sketches root', type=click.Path(exists=True),
+              help='Directory containing the sketchy test sketch1 image files.')
+def sketchy_test_dataset(photos_per_class, photos_root, sketches_root, test_photos_root, test_sketches_root):
+    """
+    Create the sketchy test dataset, assuring each included instance is available as both photo and sketch (for
+    multimodal training).
+    """
+    import re
+    from shutil import copyfile
+
+    for class_name in tqdm(os.listdir(photos_root), desc='Populating photos'):
+        for test_photo_path in os.listdir(os.path.join(photos_root, class_name))[:photos_per_class]:
+            if not os.path.exists(os.path.join(test_photos_root, class_name)):
+                os.makedirs(os.path.join(test_photos_root, class_name))
+            src = os.path.join(photos_root, class_name, test_photo_path)
+            dst = os.path.join(test_photos_root, class_name, test_photo_path)
+            copyfile(src, dst)
+
+    for class_name in tqdm(os.listdir(sketches_root), desc='Populating sketches'):
+        sketch_paths = os.listdir(os.path.join(sketches_root, class_name))
+        for test_photo_path in os.listdir(os.path.join(test_photos_root, class_name)):
+            if not os.path.exists(os.path.join(test_sketches_root, class_name)):
+                os.makedirs(os.path.join(test_sketches_root, class_name))
+            same_instance_sketch = next(path for path in sketch_paths if re.search(test_photo_path.split('.')[0], path))
+            src = os.path.join(sketches_root, class_name, same_instance_sketch)
+            dst = os.path.join(test_sketches_root, class_name, test_photo_path)
+            copyfile(src, dst)
